7f11db134037b7bf4ac3d6c5f1cdeda444124cd7
ELVO19 <juniorelvo1@gmail.com>
2026-07-07 12:21:27 +0300

    Auto-load data on deploy

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py load_allowed_voters
+python manage.py load_candidates
+python manage.py createsuperuser --noinput || true
